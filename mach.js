81f416ad6140f8795e36ae46e4da404244741a0e
Mach1982 <mach1982@gmail.com>
2016-07-16 12:26:39 +0100

    Update mach.js

--- mach.js
+++ mach.js
@@ -1,7 +1,17 @@
-function portkey() {
-    window.open("https://www.pottermore.com/");
+function fb() {
+    window.open("https://www.facebook.com/mach1982");
+}
+function twit() {
+    window.open("http://www.twitter.com/mach1982");
+  }
+
+function git() {
+    window.open("https://github.com/mach1982/");
 }
 
+function linked() {
+    window.open("https://ie.linkedin.com/in/ailin-machugh-6163244a");
+}
 
 
 
