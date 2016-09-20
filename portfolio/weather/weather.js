99c4bf4888efd762bc77e60c3b642068bb902598
Mach1982 <mach1982@gmail.com>
2016-09-20 14:37:22 +0100

    Update weather.js

--- portfolio/weather/weather.js
+++ portfolio/weather/weather.js
@@ -3,10 +3,7 @@ $(document).ready(function(){
  
  
  
- var rain="";
- var cloudy="http://www.familyandhome.org/sites/g/files/g20561/f/clouds.jpg";
- var clear;
- var snow";
+
  
  $('.jumbotron').css("background-image", "url(http://www.familyandhome.org/sites/g/files/g20561/f/clouds.jpg)");
  
