fced1dcb4003fa0e233243220c625c882bb91b83
mach <mach1982@gmail.com>
2016-09-22 09:20:35 +0100

    updated weather

--- portfolio/weather/weather.js
+++ portfolio/weather/weather.js
@@ -1,6 +1,6 @@
 
 $(document).ready(function(){
- 
+
   var country;
   var temp_c;
   var temp_f;
@@ -10,73 +10,81 @@ $(document).ready(function(){
   var long;
   var icon;
   var weather;
-  
+
   $.getJSON("http://ip-api.com/json", function(data){
   lat=data.lat;
   long=data.lon;
   city=data.city;
-  
-    
+
+
  country=data.countryCode;
-    
+
      $.getJSON("https://crossorigin.me/http://api.openweathermap.org/data/2.5/weather?lat="+lat+"&lon="+long+"&units=imperial&APPID=64c45a572899067dd41b4e88a80725a5",function(jsonf){
-      
+
        temp_f=JSON.stringify(jsonf.main.temp);
        //alert(temp_f);
-     
+
    });
-       
-       
-    
+
+
+
   $.getJSON("https://crossorigin.me/http://api.openweathermap.org/data/2.5/weather?lat="+lat+"&lon="+long+"&units=metric&APPID=64c45a572899067dd41b4e88a80725a5",function(json){
     console.log(lat);
      console.log(long);
-    
-    temp_c = JSON.stringify(json.main.temp);
+
+    //temp_c = JSON.stringify(json.main.temp);
+    temp_c ==13;
     //console.log(temp_c);
-    
+
+
+
+
+
     icon = json.weather[0].icon;
     //alert(icon);
-    
+
     weather=json.weather[0].description;
     //alert(weather);
-   
+
     var unit="C";
     var temp=temp_c;
-    
+
+    if()
+
     $("#but_f").click(function(){
       unit="F";
       temp =temp_f;
       $("#temp").html(+temp+"       \u00B0"+unit +"\u2003");
-      
+
     });
-    
+
     $("#but_c").click(function(){
      temp=temp_c;
      unit="C";
      $("#temp").html(+temp+"       \u00B0"+unit +"\u2003");
-      
+
     });
-    
+
  $("#temp").append(+temp+"       \u00B0"+unit +"\u2003");
-    
-  var url="http://www.geognos.com/api/en/countries/flag/"+country+".png"; 
+
+  var url="http://www.geognos.com/api/en/countries/flag/"+country+".png";
   var icon_url="http://openweathermap.org/img/w/"+icon+".png"
-   
-    
- $("#flag").append('<img src="' + url + '" style="width:90px;height:50px;"/>'); 
-    
+
+  
+
+ $("#flag").append('<img src="' + url + '" style="width:90px;height:50px;"/>');
+
  $("#icon").append('<img src="' + icon_url + '" style="width:90px;height:50px;"/>')
-    
+
  $("#city").append(city);
- 
+
     $("#weather").append(weather);
-    
-    
-   
+
+
+
 
   });
-  
-  
+
+
   });
 });
