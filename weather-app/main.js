d5483bbe0bf937b5fb0eb9f6f159b4489bd6f9d8
mach1982 <mach1982@gmail.com>
2023-10-09 16:02:11 +0100

    Upated weather app to user aysnc fetch and add input

--- weather-app/main.js
+++ weather-app/main.js
@@ -1,61 +1,71 @@
 
-
-//const city  = document.getElementById("cityInput").value;
-
-
- const city = document.getElementById("cityInput").value;
- const defaultCity ="Dublin";
-
+/* // function to get city name   */ 
+function getCity (){
+  const city=document.getElementById("cityInput").value;
+  fetchWeather(city)
  
-
-
-fetch(`https://api.weatherapi.com/v1/current.json?key=1499bed3ace74172a3b215054231109&q=${city}`, { mode: 'cors' })
-  .then(function (response) {
-    // Check if the response status is OK (200) before attempting to parse the JSON
-    if (!response.ok) {
-      throw new Error('Network response was not ok');
-    }
-    return response.json(); // This returns a Promise
-  })
-  .then(function (response) {
-    // Now you can work with the parsed JSON data
-
-    
-
-    
-    
-
-    console.log(response);
-    
-    const loc = response.location.name;
-    const temp_c = response.current.temp_c;
-    const imageIcon = response.current.condition.icon;
-    const imageText = response.current.condition.text;
-
-    const h1Element = document.createElement('h1');
-    h1Element.innerHTML += `
-    
-                          ${loc}
-                          <br />
-                          <br />
-                          ${temp_c}\u2103  
-                          <br />
-                          <br />
-                          <img src="${imageIcon}" alt="${imageText}">
-                          <br />
-                          <br />
-                          ${imageText}
-                          `;
-
-    const manDiv = document.getElementById('app');
-    manDiv.appendChild(h1Element)
-
-   
-  })
-  .catch(function (error) {
-    console.error('There was a problem with the fetch operation:', error);
-  });
-
+ }
+ 
+ async function fetchWeather(city){
+ 
+  /*  // connecting api */
+ const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=1499bed3ace74172a3b215054231109&q=${city}`,{ mode: 'cors' });
+     /* // if the response is no a 200 throw network error */
+     if (!response.ok) {
+       throw new Error('Network response was not ok');
+     }
+     /*  getting weather data using the await promise */
+     const weatherData = await response.json();
+ 
+     console.log(weatherData);
+     
+ 
+     const loc = weatherData.location.name;
+     const temp_c = weatherData.current.temp_c;
+     const imageIcon = weatherData.current.condition.icon;
+     const imageText = weatherData.current.condition.text;
+     console.log(loc);
+ 
+     /* Creating H1 element to display the data */
+     const h1Element = document.createElement('h1');
+     h1Element.innerHTML = `
+                           
+                           ${loc}
+                           <br />
+                           <br />
+                           ${temp_c}\u2103  
+                           <br />
+                           <br />
+                           <img src="${imageIcon}" alt="${imageText}">
+                           <br />
+                           <br />
+                           ${imageText}
+                           
+                           `;
+ 
+     const manDiv = document.getElementById('app');
+     /* adding city input field  */
+     manDiv.innerHTML = `<input type="text" id="cityInput" placeholder="Enter Location">
+     <button onclick="getCity()">Get Weather</button>` 
+     /* Appending h1 to DOM */
+     manDiv.appendChild(h1Element);
+     
+ 
+   }
+ 
+     
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+  
+ 
+ 
+ 
 
 
 
